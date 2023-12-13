8b6f925697d054a88a740f66fbc4671ac8a1cec7
itsmiraz <mirajulislamofficial044@gmail.com>
2023-12-13 19:44:21 +0600

    final changes

--- src/app/modules/course/Course.servicee.ts
+++ src/app/modules/course/Course.servicee.ts
@@ -150,11 +150,10 @@ const getAllcoursesFromDb = async (query: Record<string, unknown>) => {
   }
 
   const levelQuery = await durationInWeeksQuery.find(level);
-  const allCourse = await Course.find({});
   const metaData = {
     page: page,
     limit: limit,
-    total: allCourse.length,
+    total: levelQuery.length,
   };
   return {
     metaData,
