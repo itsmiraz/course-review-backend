bb5fe3177fb8a044189e87fca87a4f1a1e5d8e74
itsmiraz <mirajulislamofficial044@gmail.com>
2023-12-24 21:40:03 +0600

    zod validation added for user

---
+++ src/app/modules/user/User.validation.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+const createStringSchema = (fieldName: string) =>
+  z.string({
+    invalid_type_error: `${fieldName} must be a string`,
+    required_error: `${fieldName} is required`,
+  });
+const createUserValidationSchema = z.object({
+  body: z.object({
+    username: createStringSchema('User Name'),
+    email: createStringSchema('Email'),
+    password: createStringSchema('Password').min(
+      6,
+      'Password Must be minimum 6 characters',
+    ),
+    role: z.enum(['user', 'admin']),
+  }),
+});
+
+export const UserValidations = {
+  createUserValidationSchema,
+};
